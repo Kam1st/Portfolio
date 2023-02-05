92003f6a4fa69640641eb42114532f38ac4f7979
Kam1st <kam.hatteea@hotmail.com>
2023-02-05 13:42:40 -0500

    changes aos edu

--- src/components/educationPage.vue
+++ src/components/educationPage.vue
@@ -1,7 +1,7 @@
 <template>
   <br>
 
-  <aos-vue animation="zoom-in-up" data-aos-duration="900">
+  <aos-vue animation="fade-down" data-aos-duration="900">
     <h2 style="color: #5F5E43;">Champlain College Saint-Lambert</h2>
     <p style="color: #5F5E43;">At Champlain College, I am on my way to complete a 3-year Computer Science technical DEC. <br>
       This program taught me a lot about the world of computers and software programming (and is the reason I can code this
