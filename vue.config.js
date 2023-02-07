7284f42394bc41fe117281f52d2cd8056ee94f51
Kam1st <kam.hatteea@hotmail.com>
2023-02-07 14:43:28 -0500

    idk maybe this one

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   publicPath: '/kamilah-portfolio/',
   outputDir: './dist',
   assetsDir: "./src",
   indexPath: "./public/index.html"
-}
+})
