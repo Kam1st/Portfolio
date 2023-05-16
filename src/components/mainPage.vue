781b3719032b0f78a842223028c065aab266d797
Kam1st <kam.hatteea@hotmail.com>
2023-05-16 07:21:42 -0400

    new

--- src/components/mainPage.vue
+++ src/components/mainPage.vue
@@ -90,35 +90,43 @@
     <div style="background-color: black">
       <br>
 <h1>Education And Projects</h1>
-      <br>
+<br>
     <aos-vue animation="fade" data-aos-duration="600">
-      <h2 >Champlain College Saint-Lambert</h2>
-      <div>
-        <img src="@/assets/edu/champ.jpg" width="800" height="450" style="border-radius: 5%;"/>
-      </div>
-      <div style="background-color: #383630; margin-right: 350px; margin-left: 350px; border-radius: 3%; margin-top: 0px;">
-        <h3>(2020-2023)</h3>
+      <h2>Champlain College Saint-Lambert</h2>
+      <br>
+        <div class="row1">
+          <div class="column">
+          <img src="@/assets/edu/champ.jpg" width="650" height="350" style="border-radius: 1%; margin-left: 50px"/>
+          <h3>(2020-2023)</h3>
+          </div>
+
+      <div class="column" style="margin-right: 150px; margin-left: 150px;margin-top: 0px; flex: 5%;">
+        <br><br><br><br><br>
         <p>At Champlain College, I am on my way to complete a 3-year Computer Science technical DEC. <br>
           This program taught me a lot about the world of computers and software programming <br> (and is the reason I can code this
           wonderful website all by myself:)).
         </p>
           <a href="https://www.champlainsaintlambert.ca/">Visit the college's site</a>
-      </div>
+        </div>
+        </div>
     </aos-vue>
 
     <br>
     <br>
     <aos-vue animation="fade-down">
-      <div>
-        <h2>Universal Marketing Inventory System</h2>
+      <h2>Universal Marketing Inventory System</h2>
+      <br>
+      <br>
+      <div class="row1">
         <br>
+      <div clas="column" style="flex: 5%;">
+        <img src="@/assets/projects/um.png" width="600" height="450" style="border-radius: 1%;"/>
+        <h3>(2023)</h3>
       </div>
-
-      <div>
-        <img src="@/assets/projects/um.png" width="800" height="500" style="border-radius: 5%;"/>
-        <div style="background-color: #383630; margin-right: 350px; margin-left: 350px; border-radius: 3%; margin-top: 0px">
+      <div class="column" style="flex: 5%;">
+        <div style="margin-right: 150px; margin-left: 100px; border-radius: 3%; margin-top: 0px">
           <p>  Universal Marketing Co. Ltd (UMCL) is a wholesale supplier for plumbing equipment to  retail  businesses  and  professionals.
-            <br>Currently,  the  company  uses  an  external Business Management service to store and retrieve data for all transactions,
+            Currently,  the  company  uses  an  external Business Management service to store and retrieve data for all transactions,
             called MYOB (Mind Your Own Business). Transactions include: <br>
 
             <br>Placing orders
@@ -136,7 +144,7 @@
         </p>
         <br>
       </div>
-
+      </div>
 
     </aos-vue>
 
@@ -144,23 +152,28 @@
 
     <aos-vue animation="fade-right">
       <h2>Adventure Coin</h2>
+      <br>
+      <br>
         <div class="row1">
-          <div class="column">
-        <img src="@/assets/projects/game_level1.png" width="800" height="500" style="border-radius: 5%;"/>
+
+        <div class="column" style="flex: 5%;">
+        <img src="@/assets/projects/game_level1.png" width="650" height="400" style="border-radius: 1%;"/>
+          <h3>(2022)</h3>
         </div>
-          <div class="column">
-        <img src="@/assets/projects/game_ending.png" width="800" height="500" style="border-radius: 5%;"/>
-          </div>
-        </div>
-        <div style="background-color: #383630; margin-right: 350px; margin-left: 350px; border-radius: 3%; margin-top: 0px; padding-top: 5px; padding-bottom: 5px">
+
+          <div class="column" style="flex: 5%;">
+        <div style="margin-right: 150px; margin-left: 150px; border-radius: 1%; margin-top: 0px;">
+          <br><br><br><br><br>
           <p>Adventure Coin is a 2D platformer made with the Unity plateform and coded in C#. <br> This project was made for my
             Game Development class with Unity and the goal was to use <br> everything learnt in class in a final game. Things
             like parallax, assets, scripts <br> and much more helped create the finished product.
           </p>
-        </div>
         <br>
         <p>Check out the complete project at <a href="https://github.com/Kam1st/adventure_coin">https://github.com/Kam1st/adventure_coin </a></p>
         <br>
+        </div>
+          </div>
+        </div>
     </aos-vue>
 
     </div>
@@ -188,12 +201,11 @@ export default {
 }
 
 .column {
-  flex: 50%;
-  padding: 6px;
+  padding: 8px;
 }
 
 img {
-  box-shadow: 2px 2px 8px white, 1px 1px 6px white;
+  box-shadow: 1px 1px 1px white, 1px 1px 1px white;
 }
 
 </style>
